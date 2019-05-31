c2db9031832f5d5bb1c019741dec77486d4123c0
Juan Pablo Calao Perez <jcalaop@gmail.com>
2019-05-31 16:56:07 -0500

    async awayt

--- src/js/home.js
+++ src/js/home.js
@@ -1,6 +1,7 @@
-const URL = 'https://randomuser.me/api/'
+const RAND_USER_URL = 'https://randomuser.me/api/'
+const MOVIE_LIST_URL = 'https://yts.am/api/v2/list_movies.json'
 
-$.ajax(URL, {
+$.ajax(RAND_USER_URL, {
   method: 'GET',
   success: function(data) {
     console.log(data)
@@ -10,11 +11,31 @@ $.ajax(URL, {
   }
 })
 
-fetch(URL)
+fetch(RAND_USER_URL)
   .then(res => res.json())
   .then(data => {
     console.log(data)
   })
   .catch(err => {
     console.log(err)
-  })
+  });
+
+// action
+// horror
+// animation
+getMoviesOfGenre('action')
+getMoviesOfGenre('horror')
+getMoviesOfGenre('animation')
+
+async function getMoviesOfGenre(genre) {
+  const API_URL = `${MOVIE_LIST_URL}?genre=${genre}`
+
+  const movieList = await fetchFromApi(API_URL)
+  console.log(genre, movieList)
+}
+
+async function fetchFromApi(url) {
+  const response = await fetch(url)
+  const data = await response.json()
+  return data
+}
